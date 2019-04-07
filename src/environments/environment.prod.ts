c2af849e6d81fbfa80b0f2a24b9d29ff15c1e057
GMCS/ikretinin <ikretinin@gmcs.ru>
2019-04-07 10:18:09 +0300

    ve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'http://84.201.138.180/api',
+  API_ENDPOINT: 'https://84.201.138.180/api',
   returnUrl: 'https://84.201.138.180.xip.io/channels'
 };
